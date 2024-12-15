e6daf273db7d906bf92c7e61d1785159617d6722
fukafu <jorge4090@proton.me>
2024-12-15 22:13:19 +0800

    fix: build error

--- src/app/ui/todos/list.tsx
+++ src/app/ui/todos/list.tsx
@@ -4,26 +4,37 @@ import { TrashIcon } from '@heroicons/react/24/solid'
 import { Task } from '@/app/lib/actions'
 
 export default function TodoList() {
-  const storedTasks = localStorage.getItem('tasks')
-  const [tasks, setTasks] = useState<Task[]>((storedTasks && JSON.parse(storedTasks)) || [])
+  // if this is a real app,we may use server component and react query to get initial data.
+  // https://www.robinwieruch.de/react-fetching-data
+  const [tasks, setTasks] = useState<Task[] | null>(null)
+
+  // read tasks from local storage when component is mounted
+  // コンポーネントがマウントされたときにローカルストレージからタスクを読み込む
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks')
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks))
+    }
+  }, [])
 
   // when new task was created, update tasks to local storage
   // 新しいタスクが作成された時に、タスクをローカルストレージに更新する
   useEffect(() => {
+    if (!tasks) return
     localStorage.setItem('tasks', JSON.stringify(tasks))
   }, [tasks])
 
   const deleteTask = (id: string) => {
     // filter out the selected task by id
     // 選択されたタスクをIDで外す
-    setTasks(tasks.filter((task) => task.id !== id))
+    setTasks(tasks && tasks.filter((task) => task.id !== id))
   }
 
   const toggleTaskCompletion = (id: string) => {
     // if the task's id matches the selected task's id, toggle the completed state.Otherwise, return the task as is
     // タスクのIDが選択されたタスクのIDと一致する場合、完了状態を切り替えます
     // それ以外の場合は、タスクをそのまま返します
-    const updatedTasks = tasks.map((task) => (task.id === id ? { ...task, completed: !task.completed } : task))
+    const updatedTasks = tasks && tasks.map((task) => (task.id === id ? { ...task, completed: !task.completed } : task))
     setTasks(updatedTasks)
   }
 
@@ -39,7 +50,7 @@ export default function TodoList() {
         </tr>
       </thead>
       <tbody>
-        {tasks.map((task) => (
+        {tasks?.map((task) => (
           <tr key={task.id} className={`border-b ${task.completed ? 'line-through text-gray-500' : ''}`}>
             <td className="py-2 text-center">
               <input
